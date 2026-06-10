52385d9d6a167f5ee504dc2abfa2a0905c43e8dd
Christian Nagel <christian@christiannagel.com>
2026-06-10 17:13:27 +0200

    Initial BooksSample project with Book model and factory
    Added .NET 10 console app project BooksSample with solution and project files. Implemented Book class with required properties and BooksFactory for sample data. Included basic Program.cs with "Hello, World!" output.

---
+++ copilot/BooksSample/BooksSample/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksSample;
+
+public class Book
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required string Author { get; set; }
+
+
+}
---
+++ copilot/BooksSample/BooksSample/BooksFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksSample;
+
+internal class BooksFactory
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Book> GetSampleBooks()
+    {
+        return new List<Book>
+        {
+            new Book
+            {
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                Description = "A novel about the American dream and the decadence of the Jazz Age."
+            },
+            new Book
+            {
+                Title = "To Kill a Mockingbird",
+                Author = "Harper Lee",
+                Description = "A novel about racial injustice in the Deep South."
+            },
+            new Book
+            {
+                Title = "1984",
+                Author = "George Orwell",
+                Description = "A dystopian novel about totalitarianism and  "
+            }
+        };
+    }
+}
